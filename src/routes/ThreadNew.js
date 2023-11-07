f79215ff99fb7a45957a309ba0c8d4e844e564a2
noguful <noguchi.shin1@gmail.com>
2023-11-07 18:05:47 +0900

    スレッド作成画面からスレッドを新規で作成できるようにする

--- src/routes/ThreadNew.js
+++ src/routes/ThreadNew.js
@@ -1,7 +1,35 @@
 export const ThreadNew = () =>{
+
+  function handleClick() {
+
+    const data = {
+      title: document.querySelector('input[name="name"]').value,
+    };
+
+    fetch('https://railway.bulletinboard.techtrain.dev/threads', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  }
+
   return (
-    <div>
-      <p>Thread New</p>
-    </div>
-  )
+    <section>
+      <h2>スレッド新規作成</h2>
+      <form>
+        <input type="text" name="name" />
+        <button type="button" onClick={handleClick}>作成</button>
+      </form>
+    </section>
+  );
 }
