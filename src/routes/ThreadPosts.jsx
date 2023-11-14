46d890fd841056a35818f5b69fca6cdb18f892f1
noguful <noguchi.shin1@gmail.com>
2023-11-15 08:56:28 +0900

    メッセージ投稿フォーム作成と投稿一覧の更新

--- src/routes/ThreadPosts.jsx
+++ src/routes/ThreadPosts.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 
 export const ThreadPosts = () => {
@@ -6,12 +6,16 @@ export const ThreadPosts = () => {
   const { threadId } = useParams();
   const [ posts, setPosts] = useState();
 
-  useEffect(() => {
+  const fetchData = () => {
     fetch(`https://railway.bulletinboard.techtrain.dev/threads/${ threadId }/posts?offset=0`)
     .then(res => res.json())
     .then(data => {
       setPosts(data)
     })
+  }
+
+  useEffect(() => {
+    fetchData();
   },[ threadId ]);
 
   const postList = posts?.posts;
@@ -22,12 +26,46 @@ export const ThreadPosts = () => {
     </li>
   );
 
+  const textAreaRef = useRef(null);
+
+  const handleClick = () => {
+
+    const data = {
+      post: textAreaRef.current?.value,
+    }
+
+    fetch(`https://railway.bulletinboard.techtrain.dev/threads/ce67b37a-d805-4695-aa06-39c2cd62e560/posts`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log('Success:', data);
+      fetchData();
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  }
+
   return (
-    <section>
+    <section className='post-wrapper'>
       <h2>ポストタイトル</h2>
-      <ul className="post-list">
-        {listItems}
-      </ul>
+      <div className='post-contents'>
+        <ul className="post-list">
+          {listItems}
+        </ul>
+        <div className='post-form'>
+          <form>
+            <textarea name="name" className='' placeholder='投稿しよう！' rows='4' ref={textAreaRef} />
+            <button type="button" className='button' onClick={handleClick}>投稿</button>
+          </form>
+        </div>
+      </div>
     </section>
   );
 }
